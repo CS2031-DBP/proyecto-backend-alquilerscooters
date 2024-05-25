d950cc767c31737ba87bf8050df2d2bf22b19fa7
Luis23345432 <luis.lopez.c@utec.edu.pe>
2024-05-25 10:58:52 -0500

    zz

--- src/main/java/com/example/alquiler_scooters/scooter/domain/Scooter.java
+++ src/main/java/com/example/alquiler_scooters/scooter/domain/Scooter.java
@@ -1,4 +1,26 @@
-package com.example.alquiler_scooters.scooter.domain;
+package com.example.alquiler_scooters.usuario.domain;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "SCOOTERS")
+@Data
 public class Scooter {
+
+@Id
+@GeneratedValue(strategy = GenerationType.IDENTITY)
+private Long id;
+
+private String estado;
+
+private String email;
+
+private String telefono;
+
+private String estado;
+
+private LocalDate fechaRegistro = LocalDate.now();
 }
