1ee2644710be0db3f1087cc68413af54eca430c9
ASUS <estefano.roque@utec.edu.pe>
2024-05-26 01:13:02 -0500

    hice la recompensa y ya lo probe en el postman pero de todas forman veanlo para que todo este correctoo.

--- src/main/java/com/example/alquiler_scooters/recompensa/application/RecompensaController.java
+++ src/main/java/com/example/alquiler_scooters/recompensa/application/RecompensaController.java
@@ -4,12 +4,11 @@ package com.example.alquiler_scooters.recompensa.application;
 import com.example.alquiler_scooters.recompensa.domain.Recompensa;
 import com.example.alquiler_scooters.recompensa.domain.RecompensaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/recompensas")
