400c3f3cb90ca5b40b4271b14bdeca027e4e7d46
d4vidmanu <david.silva.m@utec.edu.pe>
2024-07-07 14:32:36 -0500

    Update profile

--- src/main/java/com/example/alquiler_scooters/auth/AuthImpl.java
+++ src/main/java/com/example/alquiler_scooters/auth/AuthImpl.java
@@ -1,8 +1,5 @@
 package com.example.alquiler_scooters.auth;
 
-import com.example.alquiler_scooters.config.JwtService;
-import com.example.alquiler_scooters.usuario.domain.Role;
-import com.example.alquiler_scooters.usuario.domain.Usuario;
 import com.example.alquiler_scooters.usuario.domain.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -12,9 +9,6 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class AuthImpl {
-
-    @Autowired
-    private UsuarioService userService;
 
     public String getCurrentEmail() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
